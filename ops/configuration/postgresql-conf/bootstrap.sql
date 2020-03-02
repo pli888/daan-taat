952c7d0fe8d1cab679ac727579ee92f38f0a0223
Peter Li <peter@gigasciencejournal.com>
2020-03-02 19:23:48 +0000

    Add primary keys to table and column tables

--- ops/configuration/postgresql-conf/bootstrap.sql
+++ ops/configuration/postgresql-conf/bootstrap.sql
@@ -12,12 +12,13 @@ INSERT INTO database(name, host, port, username, password, imported) VALUES ('db
 INSERT INTO database(name, host, port, username, password, imported) VALUES ('db2', 'localhost', '54321', 'gigadb', 'vagrant', FALSE);
 
 CREATE TABLE "table" (
-    id SERIAL PRIMARY KEY ,
+    id SERIAL PRIMARY KEY,
     database_id INTEGER NOT NULL REFERENCES database(id),
     name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE "column" (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     type VARCHAR(50) NOT NULL,
     table_id INTEGER NOT NULL REFERENCES "table"(id)
